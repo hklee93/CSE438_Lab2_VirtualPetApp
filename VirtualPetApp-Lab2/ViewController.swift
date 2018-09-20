1b38a4152f6c289009b43e4e6caf5d5b47654808
Hakkyung <hakkyung@wustl.edu>
2018-09-20 17:06:39 -0500

    Initial Commit

---
+++ VirtualPetApp-Lab2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VirtualPetApp-Lab2
+//
+//  Created by Hakkyung on 2018. 9. 20..
+//  Copyright © 2018년 Hakkyung Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
